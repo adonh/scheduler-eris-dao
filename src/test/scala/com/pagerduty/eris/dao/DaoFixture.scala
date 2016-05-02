0d8e75848245051389aba7462a5ca7d470e8bfe6
Owen Kim <owen@pagerduty.com>
2016-05-02 11:38:12 -0700

    Refactor mkClusterCtx in DaoFixture

--- src/test/scala/com/pagerduty/eris/dao/DaoFixture.scala
+++ src/test/scala/com/pagerduty/eris/dao/DaoFixture.scala
@@ -1,28 +1,29 @@
 package com.pagerduty.eris.dao
 
 import com.netflix.astyanax.connectionpool.NodeDiscoveryType
-import com.netflix.astyanax.connectionpool.impl.{ConnectionPoolConfigurationImpl, CountingConnectionPoolMonitor}
+import com.netflix.astyanax.connectionpool.impl.{ ConnectionPoolConfigurationImpl, CountingConnectionPoolMonitor }
 import com.netflix.astyanax.impl.AstyanaxConfigurationImpl
-import com.netflix.astyanax.{Cluster, Keyspace}
+import com.netflix.astyanax.{ Cluster, Keyspace }
 import com.pagerduty.eris.ClusterCtx
 import com.pagerduty.eris.schema.SchemaLoader
-import org.scalatest.{Outcome, fixture}
-
+import org.scalatest.{ Outcome, fixture }
 
 trait DaoFixture { this: fixture.Suite =>
   type FixtureParam <: Dao
   protected def mkFixtureDao(cluster: Cluster, keyspace: Keyspace): FixtureParam
 
+  protected def mkClusterCtx: ClusterCtx = new ClusterCtx(
+    clusterName = "CassCluster",
+    astyanaxConfig = new AstyanaxConfigurationImpl()
+      .setDiscoveryType(NodeDiscoveryType.RING_DESCRIBE),
+    connectionPoolConfig = new ConnectionPoolConfigurationImpl("CassConnectionPool")
+      .setSeeds("localhost:9160")
+      .setPort(9160),
+    connectionPoolMonitor = new CountingConnectionPoolMonitor()
+  )
+
   def withFixture(test: OneArgTest): Outcome = {
-    val context = new ClusterCtx(
-      clusterName = "CassCluster",
-      astyanaxConfig = new AstyanaxConfigurationImpl()
-        .setDiscoveryType(NodeDiscoveryType.RING_DESCRIBE),
-      connectionPoolConfig = new ConnectionPoolConfigurationImpl("CassConnectionPool")
-        .setSeeds("localhost:9160")
-        .setPort(9160),
-      connectionPoolMonitor = new CountingConnectionPoolMonitor()
-    )
+    val context = mkClusterCtx
     val cluster = context.cluster
 
     val keyspaceName = getClass.getSimpleName + Thread.currentThread.getId
